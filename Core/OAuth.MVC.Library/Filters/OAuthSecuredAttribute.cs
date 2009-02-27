f0e8919f5816c556ff4f78682914b223f6a6ee84
Owen Evans <owen@iouk.com>
2009-02-28 12:39:52 +1300

    added licence.txt

--- Core/OAuth.MVC.Library/Filters/OAuthSecuredAttribute.cs
+++ Core/OAuth.MVC.Library/Filters/OAuthSecuredAttribute.cs
@@ -1,9 +1,9 @@
 
 using System.Web.Mvc;
-using System.Linq;
+
 namespace OAuth.MVC.Library.Filters
 {
-  public class OAuthSecuredAttribute : ActionFilterAttribute, IAuthorizationFilter,IResultFilter
+  public class OAuthSecuredAttribute : ActionFilterAttribute, IAuthorizationFilter
   {
     
 
