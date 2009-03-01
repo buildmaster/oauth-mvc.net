69d64765e356729f9583a151b075f63176e5e98e
Owen Evans <owen@iouk.com>
2009-03-02 09:34:11 +1300

    fixed up sample site

--- Sample/OAuth.MVC.Sample/Global.asax.cs
+++ Sample/OAuth.MVC.Sample/Global.asax.cs
@@ -1,7 +1,15 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Web.Mvc;
 using Codeclimber.Ninject.FilterInjector;
 using System.Web.Routing;
+using DevDefined.OAuth.Framework;
+using DevDefined.OAuth.Provider;
+using DevDefined.OAuth.Provider.Inspectors;
+using DevDefined.OAuth.Storage;
+using DevDefined.OAuth.Testing;
 using Ninject.Core;
 using Ninject.Framework.Mvc;
 using OAuth.MVC.Library.Controllers;
@@ -29,12 +37,189 @@ namespace OAuth.MVC.Sample
 
     protected override IKernel CreateKernel()
     {
-
-      return new StandardKernel(new AutoControllerModuleWithFilters(Assembly.GetExecutingAssembly(),typeof(OAuthController).Assembly));
+      return new StandardKernel(new AutoControllerModuleWithFilters(Assembly.GetExecutingAssembly(),typeof(OAuthController).Assembly),new SampleModuler());
     }
 
    
   }
 
- 
+  internal class SampleModuler : StandardModule
+  {
+    public override void Load()
+    {
+      Bind<IOAuthContextBuilder>().To<OAuthContextBuilder>();
+      var nonceStoreInspector = new NonceStoreInspector(new TestNonceStore());
+      var consumerStore = new TestConsumerStore();
+      var signatureInspector = new SignatureValidationInspector(consumerStore);
+      var consumerValidationInspector = new ConsumerValidationInspector(consumerStore);
+      var timestampInspector = new TimestampRangeInspector(new TimeSpan(0, 1, 0));
+      var tokenStore = new SampleMemoryTokenStore(new TokenRepository());
+      var oauthProvider = new OAuthProvider(tokenStore,consumerValidationInspector, nonceStoreInspector,timestampInspector, signatureInspector);
+      Bind<IOAuthProvider>().ToConstant(oauthProvider);
+    }
+  }
+
+  internal class SampleConsumerStore:IConsumerStore
+  {
+    public Dictionary<string,string> secrets = new Dictionary<string, string>();
+    public bool IsConsumer(IConsumer consumer)
+    {
+      return true;
+    }
+
+    public void SetConsumerSecret(IConsumer consumer, string consumerSecret)
+    {
+      secrets[consumer.ConsumerKey] = consumerSecret;
+    }
+
+    public string GetConsumerSecret(IConsumer consumer)
+    {
+      if(!secrets.ContainsKey(consumer.ConsumerKey))
+        secrets[consumer.ConsumerKey] = consumer.ConsumerKey + "Secret";
+      return consumer.ConsumerKey;
+    }
+
+    public void SetConsumerCertificate(IConsumer consumer, X509Certificate2 certificate)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public X509Certificate2 GetConsumerCertificate(IConsumer consumer)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+
+  internal class SampleNonceStore:INonceStore
+  {
+    readonly Dictionary<string,List<string>> noncesForConsumer = new Dictionary<string, List<string>>();
+    public bool RecordNonceAndCheckIsUnique(IConsumer consumer, string nonce)
+    {
+      if(nonce==null||consumer.ConsumerKey==null)
+        return true;
+      if (!noncesForConsumer.ContainsKey(consumer.ConsumerKey))
+        noncesForConsumer[consumer.ConsumerKey] = new List<string>();
+      if(noncesForConsumer[consumer.ConsumerKey].Contains(nonce))
+        return false;
+      else
+      {
+        noncesForConsumer[consumer.ConsumerKey].Add(nonce);
+        return true;
+      }
+    }
+  }
+
+  internal class SampleMemoryTokenStore:ITokenStore
+  {
+ readonly TokenRepository _repository;
+
+ public SampleMemoryTokenStore(TokenRepository repository)
+    {
+      _repository = repository;
+    }
+
+    #region ITokenStore Members
+
+    public IToken CreateRequestToken(IOAuthContext context)
+    {
+      var token = new RequestToken
+        {
+          ConsumerKey = context.ConsumerKey,
+          Realm = context.Realm,
+          Token = Guid.NewGuid().ToString(),
+          TokenSecret = Guid.NewGuid().ToString()
+        };
+
+      _repository.SaveRequestToken(token);
+
+      return token;
+    }
+
+    public void ConsumeRequestToken(IOAuthContext requestContext)
+    {
+      RequestToken requestToken = _repository.GetRequestToken(requestContext.Token);
+
+      if (requestToken.UsedUp)
+      {
+        throw new OAuthException(requestContext, OAuthProblems.TokenRejected,
+                                 "The request token has already be consumed.");
+      }
+
+      requestToken.UsedUp = true;
+
+      _repository.SaveRequestToken(requestToken);
+    }
+
+    public void ConsumeAccessToken(IOAuthContext accessContext)
+    {
+      AccessToken accessToken = _repository.GetAccessToken(accessContext.Token);
+
+      if (accessToken.ExpireyDate < DateTime.Now)
+      {
+        throw new OAuthException(accessContext, OAuthProblems.TokenExpired,
+                                 "Token has expired (they're only valid for 1 minute)");
+      }
+    }
+
+    public IToken GetAccessTokenAssociatedWithRequestToken(IOAuthContext requestContext)
+    {
+      RequestToken request = _repository.GetRequestToken(requestContext.Token);
+      return request.AccessToken;
+    }
+
+    public RequestForAccessStatus GetStatusOfRequestForAccess(IOAuthContext accessContext)
+    {
+      RequestToken request = _repository.GetRequestToken(accessContext.Token);
+
+      if (request.AccessDenied) return RequestForAccessStatus.Denied;
+
+      if (request.AccessToken == null) return RequestForAccessStatus.Unknown;
+
+      return RequestForAccessStatus.Granted;
+    }
+
+    #endregion
+  }
+
+  /// <summary>
+  /// A simplistic in-memory repository for access and request token models - the example implementation of
+  /// <see cref="ITokenStore" /> relies on this repository - normally you would make use of repositories
+  /// wired up to your domain model i.e. NHibernate, Entity Framework etc.
+  /// </summary>    
+  public class TokenRepository
+  {
+    readonly Dictionary<string, AccessToken> _accessTokens = new Dictionary<string, AccessToken>();
+    readonly Dictionary<string, RequestToken> _requestTokens = new Dictionary<string, RequestToken>();
+
+    public RequestToken GetRequestToken(string token)
+    {
+      return _requestTokens[token];
+    }
+
+    public AccessToken GetAccessToken(string token)
+    {
+      return _accessTokens[token];
+    }
+
+    public void SaveRequestToken(RequestToken token)
+    {
+      _requestTokens[token.Token] = token;
+    }
+
+    public void SaveAccessToken(AccessToken token)
+    {
+      _accessTokens[token.Token] = token;
+    }
+  }
+  public class RequestToken : TokenBase
+  {
+    public bool AccessDenied { get; set; }
+    public bool UsedUp { get; set; }
+    public AccessToken AccessToken { get; set; }
+  }
+  public class AccessToken : TokenBase
+  {
+    public string UserName { get; set; }
+    public DateTime ExpireyDate { get; set; }
+  }
 }
